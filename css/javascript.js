c4ad8657155883a616f6ad6dc0652378a6285893
Yannis <yannis@jag-aelskar.de>
2011-05-28 14:21:57 +0200

    Aktiv wenn Formular aktiv

--- css/javascript.js
+++ css/javascript.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
 
+var login_aktiv = false;
+
+	$("#mini_login input").focus(function(){
+		login_aktiv = true;
+	}).blur(function(){
+		login_aktiv = false;
+	});
+
 
 	$("#login").mouseenter(function(){
 		$("#mini_login").css("margin-top", "0px");
 	});
 	
 	$("#bar").mouseleave(function(){
-		$("#mini_login").css("margin-top", "-30px");
+		if (!login_aktiv){
+			$("#mini_login").css("margin-top", "-30px");
+		}
 	});
 
 
